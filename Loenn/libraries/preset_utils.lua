882c83474b5887a7468ca016725559c6d75d8699
lilybeevee <lilybeevee@gmail.com>
2023-05-22 19:18:14 -0700

    update loenn version to 0.6.1

--- Loenn/libraries/preset_utils.lua
+++ Loenn/libraries/preset_utils.lua
@@ -16,7 +16,7 @@ function presetUtils.toLookup(tbl)
 end
 
 function presetUtils.checkVersion()
-    return meta.version == version("0.5.3") or meta.version == version("0.0.0-dev")
+    return meta.version == version("0.6.1") or meta.version == version("0.0.0-dev")
 end
 
 function presetUtils.saveSettings()
